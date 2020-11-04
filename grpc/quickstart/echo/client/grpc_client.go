eaf88a3b00f29b991f99fc19da8adcac69cf02f8
sinmetal <sinmetal@sinmetalcraft.jp>
2020-11-05 00:08:25 +0900

    IAPをgRPCで通過するSample
    Cloud Run の Document のサンプルがちょうどいい
    https://cloud.google.com/run/docs/triggering/grpc#request-auth

--- grpc/quickstart/echo/client/grpc_client.go
+++ grpc/quickstart/echo/client/grpc_client.go
@@ -10,20 +10,25 @@ import (
 
 	"github.com/sinmetal/til/grpc/quickstart/echo/echo"
 	"golang.org/x/net/context"
+	"google.golang.org/api/idtoken"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials"
+	grpcMetadata "google.golang.org/grpc/metadata"
 	//"google.golang.org/grpc/codes"
 	//healthpb "google.golang.org/grpc/health/grpc_health_v1"
 	//"google.golang.org/grpc/status"
 )
 
-const ()
+const (
+	audience = "244578309736-7m6gpcggkso1ut5l6bsc1lgdot5p0uq9.apps.googleusercontent.com"
+)
 
 var (
 	conn *grpc.ClientConn
 )
 
 func main() {
+	ctx := context.Background()
 
 	address := flag.String("host", "localhost:50051", "host:port of gRPC server")
 	insecure := flag.Bool("insecure", false, "connect without TLS")
@@ -51,13 +56,13 @@ func main() {
 		ce := credentials.NewTLS(&tlsCfg)
 		conn, err = grpc.Dial(*address, grpc.WithTransportCredentials(ce))
 	}
+
 	if err != nil {
 		log.Fatalf("did not connect: %v", err)
 	}
 	defer conn.Close()
 
 	c := echo.NewEchoServerClient(conn)
-	ctx := context.Background()
 
 	/*
 		ctx, cancel := context.WithTimeout(ctx, 1 * time.Second)
@@ -72,6 +77,21 @@ func main() {
 		}
 		log.Printf("RPC HealthChekStatus:%v", resp.GetStatus())
 	*/
+
+	// Create an identity token.
+	// With a global TokenSource tokens would be reused and auto-refreshed at need.
+	// A given TokenSource is specific to the audience.
+	tokenSource, err := idtoken.NewTokenSource(ctx, audience)
+	if err != nil {
+		panic(err)
+	}
+	token, err := tokenSource.Token()
+	if err != nil {
+		panic(err)
+	}
+
+	// Add token to gRPC Request.
+	ctx = grpcMetadata.AppendToOutgoingContext(ctx, "authorization", "Bearer "+token.AccessToken)
 
 	for i := 0; i < 10; i++ {
 		r, err := c.SayHello(ctx, &echo.EchoRequest{Name: "unary RPC msg "})
--- grpc/quickstart/echo/go.mod
+++ grpc/quickstart/echo/go.mod
@@ -5,5 +5,6 @@ go 1.15
 require (
 	github.com/golang/protobuf v1.4.3
 	golang.org/x/net v0.0.0-20201031054903-ff519b6c9102
+	google.golang.org/api v0.34.0
 	google.golang.org/grpc v1.33.1
 )
