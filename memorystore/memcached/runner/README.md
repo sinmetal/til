63348395a9277fccab678f56308429b670feea1e
sinmetal <sinmetal@sinmetalcraft.jp>
2021-01-07 20:19:20 +0900

    add memcached runner

---
+++ memorystore/memcached/runner/README.md
@@ -0,0 +1,6 @@
+# runner
+
+memorystore/memcached にRequestを送りまくってみる。
+
+* CPU利用率の動きを見たい
+* connect: connection refused が出るかを見たい
---
+++ memorystore/memcached/runner/main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+
+	"github.com/bradfitz/gomemcache/memcache"
+	"github.com/google/uuid"
+)
+
+func main() {
+	mem := memcache.New("10.14.80.3:11211")
+
+	for {
+		var wg sync.WaitGroup
+		for i := 0; i < 1000; i++ {
+			wg.Add(1)
+			go func(i int) {
+				defer wg.Done()
+				id := uuid.New().String()
+				err := mem.Add(&memcache.Item{
+					Key:   id,
+					Value: []byte(id),
+				})
+				if err != nil {
+					fmt.Println(err.Error())
+				}
+			}(i)
+		}
+	}
+}
