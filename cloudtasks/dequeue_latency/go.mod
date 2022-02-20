8552d342e0a905d9a4702a94bb6ce636a4f70db4
sinmetal <sinmetal@sinmetalcraft.jp>
2022-02-20 12:42:01 +0900

    WIP

---
+++ cloudtasks/dequeue_latency/go.mod
@@ -0,0 +1,12 @@
+module github.com/sinmetal/til/cloudtasks/dequeue_latency
+
+go 1.16
+
+require (
+	cloud.google.com/go/cloudtasks v1.2.0
+	cloud.google.com/go/compute v1.1.0
+	github.com/golang/groupcache v0.0.0-20210331224755-41bb18bfe9da // indirect
+	github.com/google/uuid v1.3.0
+	golang.org/x/net v0.0.0-20220127200216-cd36cc0744dd // indirect
+	google.golang.org/genproto v0.0.0-20220217155828-d576998c0009
+)
---
+++ cloudtasks/dequeue_latency/worker/main.go
@@ -0,0 +1,104 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"os"
+	"time"
+
+	cloudtasks "cloud.google.com/go/cloudtasks/apiv2"
+	"cloud.google.com/go/compute/metadata"
+	"github.com/google/uuid"
+	taskspb "google.golang.org/genproto/googleapis/cloud/tasks/v2"
+)
+
+var tasksClient *cloudtasks.Client
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello, World")
+}
+
+func ReceiveTaskHandler(w http.ResponseWriter, r *http.Request) {
+	body, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		w.Write([]byte(err.Error()))
+		return
+	}
+	fmt.Printf("__latency__,receiveTask,%s,%d\n", string(body), time.Now().Unix())
+}
+
+func AddTaskHandler(w http.ResponseWriter, r *http.Request) {
+	ctx := r.Context()
+
+	ip, err := metadata.ExternalIP()
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte(err.Error()))
+		return
+	}
+	pID, err := metadata.ProjectID()
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte(err.Error()))
+		return
+	}
+
+	queuePath := fmt.Sprintf("projects/%s/locations/%s/queues/%s", pID, "asia-northeast1", "latency")
+	id := uuid.New().String()
+	fmt.Printf("__latency__,addTask,%s,%d\n", id, time.Now().Unix())
+	taskName := fmt.Sprintf("%s/tasks/%s", queuePath, id)
+	// Build the Task payload.
+	// https://godoc.org/google.golang.org/genproto/googleapis/cloud/tasks/v2#CreateTaskRequest
+	req := &taskspb.CreateTaskRequest{
+		Parent: queuePath,
+		Task: &taskspb.Task{
+			Name: taskName,
+			// https://godoc.org/google.golang.org/genproto/googleapis/cloud/tasks/v2#HttpRequest
+			MessageType: &taskspb.Task_HttpRequest{
+				HttpRequest: &taskspb.HttpRequest{
+					HttpMethod: taskspb.HttpMethod_POST,
+					Url:        fmt.Sprintf("http://%s:8080/tasks/receive", ip),
+					Body:       []byte(id),
+				},
+			},
+		},
+	}
+
+	// Add a payload message if one is present.
+	req.Task.GetHttpRequest().Body = []byte(uuid.New().String())
+
+	createdTask, err := tasksClient.CreateTask(ctx, req)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte(err.Error()))
+		return
+	}
+	w.Write([]byte(createdTask.GetName()))
+	fmt.Printf("__latency__,createdTask,%s,%d\n", id, time.Now().Unix())
+}
+
+func main() {
+	ctx := context.Background()
+
+	var err error
+	tasksClient, err = cloudtasks.NewClient(ctx)
+	if err != nil {
+		panic(err)
+	}
+
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
+	fmt.Printf("Start Listen PORT:%s\n", port)
+
+	http.HandleFunc("/tasks/receive", ReceiveTaskHandler)
+	http.HandleFunc("/tasks/add", AddTaskHandler)
+	http.HandleFunc("/", handler)
+	if err := http.ListenAndServe(fmt.Sprintf(":%s", port), nil); err != nil {
+		fmt.Println(err.Error())
+	}
+}
