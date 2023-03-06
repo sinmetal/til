9ac32dd0c2005dd1ae85485a4728e060eeb07901
sinmetal <sinmetal@sinmetalcraft.jp>
2023-03-06 15:15:33 +0900

    datasetのdefaultとは異なるexpirationを設定するtableを作成

--- bq/partitioned_table_expiration/google_bigquery_table.tf
+++ bq/partitioned_table_expiration/google_bigquery_table.tf
@@ -26,3 +26,32 @@ resource "google_bigquery_table" "bar" {
 EOF
 
 }
+
+resource "google_bigquery_table" "foo" {
+  project    = "terraform20230306"
+  dataset_id = google_bigquery_dataset.first_dataset.dataset_id
+  table_id   = "foo"
+
+  time_partitioning {
+    type          = "DAY"
+    expiration_ms = 1 * 24 * 3600 * 1000
+  }
+
+  schema = <<EOF
+[
+  {
+    "name": "permalink",
+    "type": "STRING",
+    "mode": "NULLABLE",
+    "description": "The Permalink"
+  },
+  {
+    "name": "state",
+    "type": "STRING",
+    "mode": "NULLABLE",
+    "description": "State where the head office is located"
+  }
+]
+EOF
+
+}
