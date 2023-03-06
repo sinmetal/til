eab091b50cf1551bf6157e759f66a94c9aab015b
sinmetal <sinmetal@sinmetalcraft.jp>
2023-03-06 14:33:33 +0900

    fix project

--- bq/partitioned_table_expiration/google_bigquery_dataset.tf
+++ bq/partitioned_table_expiration/google_bigquery_dataset.tf
@@ -1,6 +1,7 @@
 resource "google_bigquery_dataset" "first_dataset" {
-  dataset_id                  = "first_dataset"
-  friendly_name               = "test"
-  description                 = "This is a test description"
-  location                    = "US"
+  project       = "terraform20230306"
+  dataset_id    = "first_dataset"
+  friendly_name = "test"
+  description   = "This is a test description"
+  location      = "US"
 }
