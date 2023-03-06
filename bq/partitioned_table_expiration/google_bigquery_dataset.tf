44a6036c20284aec029d1d0b9c6e337ff62bdb96
sinmetal <sinmetal@sinmetalcraft.jp>
2023-03-06 14:24:12 +0900

    remove table expiration

--- bq/partitioned_table_expiration/google_bigquery_dataset.tf
+++ bq/partitioned_table_expiration/google_bigquery_dataset.tf
@@ -3,5 +3,4 @@ resource "google_bigquery_dataset" "first_dataset" {
   friendly_name               = "test"
   description                 = "This is a test description"
   location                    = "US"
-  default_table_expiration_ms = 0
 }
