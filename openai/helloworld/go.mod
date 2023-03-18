4856fea5eed2e8ff314e1cec308302467b6ccf7d
sinmetal <sinmetal@sinmetalcraft.jp>
2023-03-18 13:59:06 +0900

    hwllo world

---
+++ openai/helloworld/go.mod
@@ -0,0 +1,5 @@
+module github.com/sinmetal/til/openai/helloworld
+
+go 1.20
+
+require github.com/sashabaranov/go-openai v1.5.2 // indirect
---
+++ openai/helloworld/helloworld_test.go
@@ -0,0 +1,11 @@
+package helloworld_test
+
+import (
+	"testing"
+
+	"github.com/sinmetal/til/openai/helloworld"
+)
+
+func TestHelloWorld(t *testing.T) {
+	helloworld.HelloWorld()
+}
